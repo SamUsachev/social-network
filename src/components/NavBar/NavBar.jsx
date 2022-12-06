8c69be30dc9f38eaac9c77688c88ffe812b4ed4a
SamUsachev <semenusachev10@gmail.com>
2022-12-06 12:17:25 +0300

    auth ajax

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import style from './NavBar.module.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import style from './NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 function NavBar() {
   return (
     <nav className={style.nav}>
       <ul className={style.text}>
         <ol>
-          <NavLink to='profile'><a className={style.but}>Profile</a> </NavLink>
+          <NavLink to="profile">
+            <a className={style.but}>Profile</a>{' '}
+          </NavLink>
         </ol>
         <ol>
-          <NavLink to='messages'><a className={style.but}>Messages</a></NavLink>
+          <NavLink to="messages">
+            <a className={style.but}>Messages</a>
+          </NavLink>
         </ol>
         <ol>
-          <a href='news' className={style.but}>News</a>
+          <NavLink to="users">
+            <a className={style.but}>Users</a>
+          </NavLink>
         </ol>
         <ol>
-          <a href='music' className={style.but}>Music</a>
+          <a href="news" className={style.but}>
+            News
+          </a>
         </ol>
         <ol>
-          <a href='settings' className={style.but}>Settings</a>
+          <a href="music" className={style.but}>
+            Music
+          </a>
+        </ol>
+        <ol>
+          <a href="settings" className={style.but}>
+            Settings
+          </a>
         </ol>
       </ul>
     </nav>
-  )
+  );
 }
-export default NavBar
+export default NavBar;
